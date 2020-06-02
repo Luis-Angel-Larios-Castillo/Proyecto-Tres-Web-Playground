7a00ae76c0fcc0276771c7451711796bddbfb303
Luis Angel Larios Castillo <luis44larios.99@gmail.com>
2020-06-01 20:32:14 -0500

    se realiza mensajeria asincrona con JavaScript

--- messenger/views.py
+++ messenger/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
-from .models import Thread
-from django.http import Http404
-
+from .models import Thread, Message
+from django.http import Http404, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 @method_decorator(login_required, name="dispatch")
@@ -22,3 +23,19 @@ class ThreadDetail(DetailView):
         if self.request.user not in obj.users.all():
             raise Http404()
         return obj
+
+def add_message(request, pk):
+    print(request.GET)
+    json_response = {'created':False}
+    if request.user.is_authenticated:
+        content = request.GET.get('content', None)
+        if content:
+            thread = get_object_or_404(Thread, pk=pk)
+            message = Message.objects.create(user=request.user, content=content)
+            thread.messages.add(message)
+            json_response['created'] = True
+    else:
+        raise Http404("User is not authenticated")
+
+
+    return JsonResponse(json_response)
